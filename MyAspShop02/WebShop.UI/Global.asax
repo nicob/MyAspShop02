d382195a6f87705b6a4147e0641fe19e59fe88a0
nikobel708 nikobel708 <info@nikosbeligiannis.com>
2020-02-17 12:48:14 +0200

    Models and storage

---
+++ MyAspShop02/WebShop.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebShop.UI.MvcApplication" Language="C#" %>
---
+++ MyAspShop02/WebShop.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebShop.UI.Startup))]
+namespace WebShop.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
